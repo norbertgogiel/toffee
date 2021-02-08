edb28ad2c50bb6cbebfdc182e8c773c6a85a9e9a
Norbert Gogiel <norbert.gogiel@fisglobal.com>
2021-02-08 00:10:45 +0000

    Add interval scheduling for tasks

--- src/main/java/com/norbertgogiel/toffee/IntervalScheduledTaskAgent.java
+++ src/main/java/com/norbertgogiel/toffee/IntervalScheduledTaskAgent.java
@@ -1,15 +1,18 @@
 package com.norbertgogiel.toffee;
 
 
+import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class IntervalScheduledTaskAgent {
 
     private ScheduledThreadPoolExecutor taskAgent;
+    private ScheduledThreadPoolExecutor shutdownAgent;
 
     public IntervalScheduledTaskAgent(int corePoolSize) {
         taskAgent = new ScheduledThreadPoolExecutor(corePoolSize);
+        shutdownAgent = new ScheduledThreadPoolExecutor(corePoolSize);
     }
 
     public int getCorePoolSize() {
@@ -24,7 +27,8 @@ public class IntervalScheduledTaskAgent {
         return taskAgent.getActiveCount();
     }
 
-    public void submit(Runnable runnable, long initDelay, long delay, TimeUnit timeUnit) {
-        taskAgent.scheduleAtFixedRate(runnable, initDelay, delay, timeUnit);
+    public void submit(Runnable runnable, long initDelay, long period, long delayToShutdown, TimeUnit timeUnit) {
+        Future<?> future =  taskAgent.scheduleAtFixedRate(runnable, initDelay, period, timeUnit);
+        shutdownAgent.schedule(() -> future.cancel(false), delayToShutdown, timeUnit);
     }
 }
