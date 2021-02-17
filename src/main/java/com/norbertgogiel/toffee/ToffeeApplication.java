0d62795968f1b1730d90e4b992bee9815d662ad7
Norbert Gogiel <norbert.gogiel@fisglobal.com>
2021-02-17 23:38:51 +0000

    Create annotations to allow schedules being differentiated

--- src/main/java/com/norbertgogiel/toffee/ToffeeApplication.java
+++ src/main/java/com/norbertgogiel/toffee/ToffeeApplication.java
@@ -1,14 +1,33 @@
 package com.norbertgogiel.toffee;
 
+import com.norbertgogiel.toffee.annotations.IntervalScheduled;
+import com.norbertgogiel.toffee.annotations.ScheduledFrom;
+import com.norbertgogiel.toffee.annotations.ScheduledUntil;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class ToffeeApplication {
 
     public void init(Class<?> source) {
         assertNotNull(source);
+        if (source.isAnnotationPresent(IntervalScheduled.class)) {
+            Arrays.stream(source.getMethods())
+                    .forEach(method -> {
+                        if (method.isAnnotationPresent(ScheduledFrom.class)
+                        && method.isAnnotationPresent(ScheduledUntil.class))
+                                processScheduled(method);
+            });
+        }
     }
 
     private static void assertNotNull(Object object) {
         if (object == null) {
             throw new IllegalArgumentException("Object must not be null");
         }
+    }
+
+    private void processScheduled(Method method) {
+        throw new UnsupportedOperationException();
     }
 }
--- src/test/java/com/norbertgogiel/toffee/TestToffeeApplication.java
+++ src/test/java/com/norbertgogiel/toffee/TestToffeeApplication.java
@@ -3,7 +3,12 @@ package com.norbertgogiel.toffee;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.norbertgogiel.toffee.annotations.IntervalScheduled;
+import com.norbertgogiel.toffee.annotations.ScheduledFrom;
+import com.norbertgogiel.toffee.annotations.ScheduledUntil;
 import org.junit.Test;
+
+import java.io.IOException;
 
 public class TestToffeeApplication {
 
@@ -22,5 +27,35 @@ public class TestToffeeApplication {
     public void testInitNullObjectAndThrows() {
         ToffeeApplication subject = new ToffeeApplication();
         assertThrows(IllegalArgumentException.class, () -> subject.init(null));
+    }
+
+    @Test
+    public void testInitIntervalScheduledAnnotatedClass() {
+        ToffeeApplication subject = new ToffeeApplication();
+        assertDoesNotThrow(() -> subject.init(IntervalScheduledClassTest.class));
+    }
+
+    @Test
+    public void testInitScheduledAnnotatedClassAndMethods() {
+        ToffeeApplication subject = new ToffeeApplication();
+        assertThrows(UnsupportedOperationException.class ,() -> subject.init(IntervalScheduledClassAndMethodsTest.class));
+    }
+
+    @IntervalScheduled
+    static class IntervalScheduledClassTest {
+
+        public void testRunnable() throws IOException {
+            throw new IOException("evil");
+        }
+    }
+
+    @IntervalScheduled
+    static class IntervalScheduledClassAndMethodsTest {
+
+        @ScheduledFrom(hour = 11, minute = 12, second = 13, nano = 14)
+        @ScheduledUntil(hour = 12, minute = 13, second = 14, nano = 15)
+        public void testRunnable() throws IOException {
+            throw new IOException("evil");
+        }
     }
 }
